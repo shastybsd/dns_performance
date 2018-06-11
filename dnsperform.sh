0ddbee3f813717aaa904576d40dd632746bea9b2
Charlie Root <root@server.souzagomes.local>
2018-06-11 14:13:07 -0300

    Adicionada informação extra

--- dnsperform.sh
+++ dnsperform.sh
@@ -13,7 +13,6 @@ set soma = 0
 foreach i ( `cat hosts.txt` )
 	echo "Tempo da consulta do host ${i}: $tempo ms"
 	echo "Numero da consulta: `expr $cont \+ 1`"
-#	echo "Tempo total: $soma"
 	set tempo = `drill $i @$1 | grep Query | cut -f4 -d" "`
 #		if ( `echo $?` != 0 ) then
 #			set soma = `expr 1000 \+ $soma`
